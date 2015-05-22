c437317a29474b29afcd4eef3775be1a0e15bbe2
blueskies9041 <sam.legus@gmail.com>
2015-05-21 17:30:03 -0700

    Folder restructure

---
+++ ProjectAwesome/Assets/ProjectAwesome/Extensions/2D Specific/FakeRotation.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// * 	The ProjectAwesome class is spread across multiple files
+/// * 	This part allows the user to do simple, 90 or 180 degree "rotations"
+///		by inverting the x and y values of an object's scale
+/// * 	The methods in this file are not compatible with actual rotation methods and should
+///		be used only during earlier learning stages.
+/// </summary>
+
+//Author : Sam Legus
+//Last Updated: 5/21/2015
+
+//Vector2 class Documentation : http://docs.unity3d.com/ScriptReference/30_search.html?q=Vector2
+//GameObject class Documentation: http://docs.unity3d.com/ScriptReference/GameObject.html
+//Unity Extension Methods Tutorial: https://unity3d.com/learn/tutorials/modules/intermediate/scripting/extension-methods
+
+
+public static partial class ProjectAwesome
+{
+	public static void FlipHorizontal(this GameObject gameObj)
+	{
+		Vector2 originalScale = gameObj.transform.localScale;
+		Vector2 flippedScale = new Vector2(originalScale.x * -1f, originalScale.y);
+		
+		gameObj.transform.localScale = flippedScale;
+	}
+	
+	public static void FlipVertical(this GameObject gameObj)
+	{
+		Vector2 originalScale = gameObj.transform.localScale;
+		Vector2 flippedScale = new Vector2(originalScale.x , originalScale.y * -1f);
+		
+		gameObj.transform.localScale = flippedScale;
+	}
+	
+	public static void LookLeft(this GameObject gameObj)
+	{
+		Vector2 originalScale = gameObj.transform.localScale;
+		Vector2 faceLeftScale = new Vector2( Mathf.Abs(originalScale.x) * -1, originalScale.y);
+		
+		gameObj.transform.localScale = faceLeftScale;
+	}
+	
+	public static void LookRight(this GameObject gameObj)
+	{
+		Vector2 originalScale = gameObj.transform.localScale;
+		Vector2 faceLeftScale = new Vector2( Mathf.Abs(originalScale.x), originalScale.y);
+		
+		gameObj.transform.localScale = faceLeftScale;  
+	}
+	
+	public static void LookUp(this GameObject gameObj)
+	{
+		Vector2 originalScale = gameObj.transform.localScale;
+		Vector2 faceLeftScale = new Vector2( originalScale.x, Mathf.Abs (originalScale.y));
+		
+		gameObj.transform.localScale = faceLeftScale;
+	}
+	
+	public static void LookDown(this GameObject gameObj)
+	{
+		Vector2 originalScale = gameObj.transform.localScale;
+		Vector2 faceLeftScale = new Vector2(originalScale.x, Mathf.Abs (originalScale.y) * -1f);
+		
+		gameObj.transform.localScale = faceLeftScale;
+	}
+}
---
+++ ProjectAwesome/Assets/ProjectAwesome/Extensions/2D Specific/Physics.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// * 	The ProjectAwesome class is spread across multiple files
+/// * 	This part provides simple physics manipulation without GetComponent calls
+///		or the need to mess with Vector2s
+/// * 	Even though it is widely considered improper, these functions directly modify the velocity of
+///		an object's Rigidbody2D instead of using AddForce for the sake of simplicity
+/// </summary>
+
+//Author : Sam Legus
+//Last Updated: 5/21/2015
+
+//Vector2 class Documentation : http://docs.unity3d.com/ScriptReference/30_search.html?q=Vector2
+//GameObject class Documentation: http://docs.unity3d.com/ScriptReference/GameObject.html
+//Rigidbody class Documentation: http://docs.unity3d.com/ScriptReference/Rigidbody2D.html
+//Unity Extension Methods Tutorial: https://unity3d.com/learn/tutorials/modules/intermediate/scripting/extension-methods
+
+public static partial class ProjectAwesome
+{
+
+	//Sets an object's velocity to zero.
+	public static void ZeroVelocity(this GameObject gameObj)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			ourRigidbody.velocity = Vector2.zero;
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Velocity not accessible.");
+		}
+	}
+	
+	//Sets an object's velocity using two floats
+	public static void SetVelocity(this GameObject gameObj, float x, float y)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			Vector2 newVelocity = new Vector2(x,y);
+			ourRigidbody.velocity = newVelocity;
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Velocity not accessible.");
+		}
+	}
+	
+	//Sets an objects horizontal velocity
+	public static void SetVelocityX(this GameObject gameObj, float x)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			Vector2 oldVelocity = ourRigidbody.velocity;
+			Vector2 newVelocity = new Vector2(x, oldVelocity.y);
+			ourRigidbody.velocity = newVelocity;
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Velocity not accessible.");
+		}
+	}
+	
+	//Sets an objects vertical velocity
+	public static void SetVelocityY(this GameObject gameObj, float y)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			Vector2 oldVelocity = ourRigidbody.velocity;
+			Vector2 newVelocity = new Vector2(oldVelocity.x, y);
+			ourRigidbody.velocity = newVelocity;
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Velocity not accessible.");
+		}
+	}
+	
+	//Toggles the sleep mode of an object's Rigidbody2D
+	public static void TogglePhysics2D(this GameObject gameObj)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			if( ourRigidbody.IsSleeping () == false)
+			{
+				ourRigidbody.Sleep ();
+			}
+			else
+			{
+				ourRigidbody.WakeUp ();
+			}
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Cannot disable physics.");
+		}
+	}
+	
+	//Puts the object's Rigidbody2D to sleep
+	public static void DisablePhysics2D(this GameObject gameObj)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			ourRigidbody.Sleep ();
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Cannot disable physics.");
+		}
+		
+	}
+	
+	//Wakes up an object's Rigidbody2D
+	public static void EnablePhysics2D(this GameObject gameObj)
+	{
+		Rigidbody2D ourRigidbody = gameObj.GetComponent<Rigidbody2D>();
+		if(ourRigidbody != null)
+		{
+			ourRigidbody.WakeUp ();
+		}
+		else
+		{
+			Debug.Log (gameObj.name + " has no Rigidbody2D component! Cannot enable physics.");
+		}
+	}
+	
+}
