e4d3828f6a9fe72c7fe23d06aa4fb917779a39eb
unknown <sam.legus@gmail.com>
2015-05-28 17:20:36 -0700

    Added code regions

--- ProjectAwesome/Assets/ProjectAwesome/AwesomeScript/AwesomeScriptTemplate.cs
+++ ProjectAwesome/Assets/ProjectAwesome/AwesomeScript/AwesomeScriptTemplate.cs
@@ -3,6 +3,19 @@ using System.Collections;
 
 public class AwesomeScriptTemplate : AwesomeScript 
 {
+	//Put variables that you want to set from the inspector inside this region
+	#region Inspector Variables
+	
+	#endregion
+	
+	//Put variables that you do not want to set from the inspector inside this region
+	#region Private Variables
+	
+	#endregion
+	
+	//All functions inside this region are automatically called by Unity
+	#region Unity Events
+	
 	//Any code written inside of Start will be called once when the object is created.
 	void Start()
 	{
@@ -20,4 +33,11 @@ public class AwesomeScriptTemplate : AwesomeScript
 	{
 		Debug.Log (self.name + " collided with " + other.gameObject.name + "!");
 	}
+	
+	#endregion
+	
+	//Put any functions that you write yourself inside this region
+	#region My Functions
+	
+	#endregion
 }
